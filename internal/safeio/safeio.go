e698fae7ea662277eaa43096ad9ae87487dfe3c1
Aleksandr Panfilov <aaa2ppp@gmail.com>
2024-05-22 19:15:08 +0300

    safeio refactoring

--- internal/safeio/safeio.go
+++ internal/safeio/safeio.go
@@ -9,89 +9,79 @@ import (
 var ErrIOTimeout = errors.New("io timeout")
 
 type Writer struct {
-	wtr       io.Writer
-	timeout   time.Duration
-	stickyErr error
+	ioOperation
 }
 
 func NewWriter(w io.Writer, timeout time.Duration) *Writer {
-	return &Writer{
-		wtr:     w,
-		timeout: timeout,
-	}
+	return &Writer{ioOperation{fun: w.Write, timeout: timeout}}
 }
 
-func (w Writer) Write(b []byte) (int, error) {
-	if w.stickyErr != nil {
-		return 0, w.stickyErr
-	}
-
-	var (
-		n   int
-		err error
-	)
-
-	done := make(chan struct{}, 1)
-	go func() {
-		n, err = w.wtr.Write(b)
-		done <- struct{}{}
-	}()
-
-	tm := time.NewTimer(w.timeout)
-	select {
-	case <-tm.C:
-		w.stickyErr = ErrIOTimeout
-		return 0, ErrIOTimeout
-	case <-done:
-		tm.Stop()
-		w.stickyErr = err
-		return n, err
-	}
+func (w *Writer) Write(b []byte) (int, error) {
+	return w.do(b)
 }
 
 type Reader struct {
-	rdr       io.Reader
-	timeout   time.Duration
-	flush     func()
-	stickyErr error
+	ioOperation
+	flush func()
 }
 
 func NewReader(r io.Reader, timeout time.Duration, flush func()) *Reader {
-	return &Reader{
-		rdr:     r,
-		timeout: timeout,
-		flush:   flush,
-	}
+	return &Reader{ioOperation{fun: r.Read, timeout: timeout}, flush}
 }
 
-func (r Reader) Read(b []byte) (int, error) {
-	if r.stickyErr != nil {
-		return 0, r.stickyErr
-	}
-
+func (r *Reader) Read(b []byte) (int, error) {
 	if r.flush != nil {
 		r.flush()
 	}
+	return r.do(b)
+}
 
-	var (
+type ioOperation struct {
+	fun       func([]byte) (int, error)
+	timeout   time.Duration
+	tm        *time.Timer
+	stickyErr error
+}
+
+func (op *ioOperation) Timeout(t time.Duration) {
+	op.timeout = t
+}
+
+func (op *ioOperation) do(b []byte) (int, error) {
+	if op.stickyErr != nil {
+		return 0, op.stickyErr
+	}
+
+	if op.timeout <= 0 {
+		return op.fun(b)
+	}
+
+	type result struct {
 		n   int
 		err error
-	)
+	}
 
-	done := make(chan struct{}, 1)
+	done := make(chan result, 1)
 	go func() {
-		n, err = r.rdr.Read(b)
-		done <- struct{}{}
+		n, err := op.fun(b)
+		done <- result{n, err}
 	}()
 
-	tm := time.NewTimer(r.timeout)
+	if op.tm == nil {
+		op.tm = time.NewTimer(op.timeout)
+	} else {
+		op.tm.Reset(op.timeout)
+	}
+
 	select {
-	case <-tm.C:
-		r.stickyErr = ErrIOTimeout
+	case <-op.tm.C:
+		op.stickyErr = ErrIOTimeout
 		return 0, ErrIOTimeout
-	case <-done:
-		tm.Stop()
-		r.stickyErr = err
-		return n, err
+	case r := <-done:
+		if !op.tm.Stop() {
+			<-op.tm.C
+		}
+		op.stickyErr = r.err
+		return r.n, r.err
 	}
 }
